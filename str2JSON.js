2461b0959ef1ac3d7766e90659f31a73412f5948
codetyphon <codetyphon@sina.com>
2015-03-11 11:58:43 +0800

    Update str2JSON.js

--- str2JSON.js
+++ str2JSON.js
@@ -1,4 +1,10 @@
-			function str2JSON(str) {
-				eval('var __pt_json='+str);
-				return __pt_json;
-			}
+function str2JSON(str) {
+	var data='';
+	if (typeof JSON !== 'undefined'){ // IE7兼容模式不存在JSON对象
+		data = JSON.parse(str);
+	}else{
+		eval('data='+str);
+	}
+	return data;
+}
+
