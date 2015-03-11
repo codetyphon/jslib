08841412f9ddc0982f421dfc0a7857f85c1e33ce
codetyphon <codetyphon@sina.com>
2015-03-11 11:44:32 +0800

    Create noiframe.js
    防嵌入

---
+++ noiframe.js
@@ -0,0 +1,2 @@
+//防嵌入
+(function(){try{if(parent!=self && (parent.document.domain!=document.domain	|| (document.referrer && !/^http(s)?:\/\/[.\w-]+\.codetyphon\.com\//i.test(document.referrer)))){throw new Error("can't be iframed");}}catch(e){debugger;window.open(location.href, "_top");}})();
